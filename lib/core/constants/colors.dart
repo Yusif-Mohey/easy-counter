007eb818a0ec8829f5724656ccd32d03fa5b4ffb
yusif-mohey <moheyy28@gmail.com>
2024-10-10 01:10:23 +0300

    add colors list

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const List<Color> appColors = [
+  Color(0xff264653),
+  Color(0xff287271),
+  Color(0xff2a9d8f),
+  Color(0xff8ab17d),
+  Color(0xffbabb74),
+  Color(0xffe9c46a),
+  Color(0xffefb366),
+  Color(0xfff4a261),
+  Color(0xffee8959),
+];
