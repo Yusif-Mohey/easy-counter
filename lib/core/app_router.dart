18ba618d7dc1781be8e7449f2be55b6813db0310
yusif-mohey <moheyy28@gmail.com>
2024-10-10 01:21:43 +0300

    create spalsh && add app router

---
+++ lib/core/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:easy_counter/features/home/presention/views/screens/home_view.dart';
+import 'package:easy_counter/features/splash/presention/views/screens/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/kHomeView';
+  static const kSplashView = '/';
+  static final router = GoRouter(routes: [
+    GoRoute(
+      path: kSplashView,
+      builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: kHomeView,
+      builder: (context, state) => const HomeView(),
+    )
+  ]);
+}
